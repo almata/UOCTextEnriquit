29ff238379dfa0a92fcb082e0c648a8a141a0839
Albert Mata <hello@albertmata.net>
2018-10-09 11:20:07 +0200

    Enllaços web

--- uoc.js
+++ uoc.js
@@ -12,6 +12,10 @@ function createHtml() {
 			tokens.splice(i, 1, "<span style='color: #" + color + "; font-family: terminal, monaco, monospace; font-size: 10pt;'>",
 				tokens[i].replace(/ /g, "&nbsp;"), "</span>");
 		} else {
+			// Canviem [xxx](yyy) per <a href='yyy'>xxx</a>.
+			var regex = /\[(.*?)\]\((.*?)\)/g;
+			tokens[i] = tokens[i].replace(regex, htmlLinkFrom);
+			
 			// Canviem ** per etiquetes <strong> i </strong> segons correspongui.
 			var strong = tokens[i].split("**");
 			for (j = strong.length - 1; j >= 0; j--) {
@@ -59,4 +63,9 @@ function copyToClipboard() {
 
 function changeCodeColor() {
 	document.getElementById("code").style.color = document.getElementById("color").value;
+}
+
+function htmlLinkFrom(match) {
+	var tokens = match.split(/[\[\(\)\]]/).filter(n => n != "");
+	return "<a href='" + tokens[1] + "'>" + tokens[0] + "</a>";
 }
