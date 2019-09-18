6c508607d007c1ed27e6e23faa5e647251ba6f7f
Albert Mata <hello@albertmata.net>
2019-09-18 13:46:02 +0200

    Versió 0.30

--- uoc.js
+++ uoc.js
@@ -5,6 +5,7 @@ function createHtml() {
 	
 	// Canviem ` per etiquetes <span> (amb format de font, mida i color) i </span> segons correspongui.
 	var tokens = string.split("`");
+
 	for (i = tokens.length - 1; i >= 0; i--) {
 		// Les tokens a considerar pels canvis són sempre les que estan en posicions senars (perquè comencem pel 0).
 		// En el cas del codi font, revisarem també les tokens que no inclouen codi per si cal aplicar <strong> o <em>.
@@ -18,58 +19,31 @@ function createHtml() {
 			tokens[i] = tokens[i].replace(regex, htmlLinkFrom);
 			if (tokens[i] != temp) { continue };
 			
-			// Canviem ** per etiquetes <strong> i </strong> segons correspongui.
-			var strong = tokens[i].split("**");
-			for (j = strong.length - 1; j >= 0; j--) {
-				if (j % 2 != 0) {
-					strong.splice(j, 1, "<strong>", strong[j], "</strong>");					
-				}
-			}
-			tokens[i] = strong.join("");
+			// Canviem ** per etiquetes <strong> i </strong>.
+			tokens[i] = addTags(tokens[i], "**", "<strong>", "</strong>");
 
-			// Canviem _ per etiquetes <em> i </em> segons correspongui.			
-		  var em = tokens[i].split("_");
-		  for (j = em.length - 1; j >= 0; j--) {
-		   	if (j % 2 != 0) {
-		   	 em.splice(j, 1, "<em>", em[j], "</em>");
-		   	}
-			}
-		  tokens[i] = em.join("");
+			// Canviem _ per etiquetes <em> i </em>.
+			tokens[i] = addTags(tokens[i], "_", "<em>", "</em>");
 
-			// Canviem # per etiquetes <span style='font-size: 18pt;'><strong> i </strong></span> segons correspongui.			
-		  var header = tokens[i].split("#");
-		  for (j = header.length - 1; j >= 0; j--) {
-		   	if (j % 2 != 0) {
-		   	 header.splice(j, 1, "<span style='font-size: 18pt;'><strong>", header[j], "</strong></span>");
-		   	}
-			}
-		  tokens[i] = header.join("");
+			// Canviem # per etiquetes <span><strong>.
+			tokens[i] = addTags(tokens[i], "#", "<span style='font-size: 18pt;'><strong>", "</strong></span>");
 		}
 	}
 
 	tokens = [].concat.apply([], tokens);
+
 	document.getElementById("second").value = tokens.join("").replace(/(?:\r\n|\r|\n)/g, "<br/>");
+	console.warn(copyToClipboard());
 }
 
-function copyToClipboard() {
-	var element = document.getElementById("second");
-	var range = document.createRange();
-	
-	element.contentEditable = true;
-	element.readOnly = false;
-	range.selectNode(element); // range.selectNodeContents(element) doesn't work in Chrome
-	
-	var selection = window.getSelection();
-	selection.removeAllRanges();
-	selection.addRange(range);
-	
-	element.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
-	
-	element.contentEditable = false;
-	element.readOnly = true;
-	
-	document.execCommand('copy');
-	document.getElementById("first").focus();
+function addTags(token, mark, openingTag, closingTag) {
+	var parts = token.split(mark);
+	for (j = parts.length - 1; j >= 0; j--) {
+		if (j % 2 != 0) {
+			parts.splice(j, 1, openingTag, parts[j], closingTag);					
+		}
+	}
+	return parts.join("");
 }
 
 function changeCodeColor() {
@@ -80,4 +54,30 @@ function changeCodeColor() {
 function htmlLinkFrom(match) {
 	var tokens = match.split(/[\[\(\)\]]/).filter(n => n != "");
 	return "<a style='color: #396;' href='" + tokens[1] + "'>" + tokens[0] + "</a>";
+}
+
+function copyToClipboard() {
+	var text = document.getElementById("second").value;
+	
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text); 
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+		textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+			document.body.removeChild(textarea);
+			document.getElementById("first").focus();
+        }
+	}
+	
 }
