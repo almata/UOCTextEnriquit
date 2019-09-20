b6f6a72dcf7d7fcdd09b2ca4fae4352a9837a18a
Albert Mata <hello@albertmata.net>
2019-09-20 12:53:25 +0200

    Versió 0.31

--- uoc.js
+++ uoc.js
@@ -3,40 +3,56 @@ function createHtml() {
 	var string = document.getElementById("first").value;
 	string = string.replace(/</g, "&lt;"); // Issue #1
 	
-	// Canviem ` per etiquetes <span> (amb format de font, mida i color) i </span> segons correspongui.
+	// Comencem dividint el text per etiquetes ` (les que delimiten codi font).
 	var tokens = string.split("`");
-
 	for (i = tokens.length - 1; i >= 0; i--) {
-		// Les tokens a considerar pels canvis són sempre les que estan en posicions senars (perquè comencem pel 0).
-		// En el cas del codi font, revisarem també les tokens que no inclouen codi per si cal aplicar <strong> o <em>.
 		if (i % 2 != 0) {
+			// A les posicions senars hi ha codi font.
 			tokens.splice(i, 1, "<span style='color: #" + color + "; font-family: terminal, monaco, monospace; font-size: 10pt;'>",
 				tokens[i].replace(/ /g, "&nbsp;"), "</span>");
 		} else {
-			// Canviem [xxx](yyy) per <a href='yyy'>xxx</a>.
-			var regex = /\[(.*?)\]\((.*?)\)/g;
-			var temp = tokens[i];
-			tokens[i] = tokens[i].replace(regex, htmlLinkFrom);
-			if (tokens[i] != temp) { continue };
-			
-			// Canviem ** per etiquetes <strong> i </strong>.
-			tokens[i] = addTags(tokens[i], "**", "<strong>", "</strong>");
-
-			// Canviem _ per etiquetes <em> i </em>.
-			tokens[i] = addTags(tokens[i], "_", "<em>", "</em>");
-
-			// Canviem # per etiquetes <span><strong>.
-			tokens[i] = addTags(tokens[i], "#", "<span style='font-size: 18pt;'><strong>", "</strong></span>");
+			// A les posicions parells hi ha tot el que no és codi font.
+			tokens[i] = translateNonCodeTags(tokens[i]);
 		}
 	}
 
-	tokens = [].concat.apply([], tokens);
-
 	document.getElementById("second").value = tokens.join("").replace(/(?:\r\n|\r|\n)/g, "<br/>");
-	console.warn(copyToClipboard());
 }
 
-function addTags(token, mark, openingTag, closingTag) {
+function translateNonCodeTags(input) {
+	var regex = /\[(.*?)\]\((.*?)\)/g;
+	var tokens = input.split(regex);
+	
+	// Si hi ha enllaços, les tokens segueixen aquest ordre:
+	// 0 el que hi ha abans del primer enllaç
+	// 1 text del primer enllaç
+	// 2 url del primer enllaç
+	// 3 el que segueix al primer enllaç
+	// 4 text del segon enllaç
+	// (etc)
+	if (tokens[0] == input) {
+		return translateSimpleTags(input);
+	} else {
+		var output = "";
+		var i = 0;
+		while (i < tokens.length - 1) {
+			output += translateSimpleTags(tokens[i]);
+			output += htmlLink(tokens[i + 1], tokens[i + 2]);
+			i += 3;
+		}
+		output += translateSimpleTags(tokens[i]);
+		return output;
+	}
+}
+
+function translateSimpleTags(text) {
+	text = translateTag(text, "**", "<strong>", "</strong>");
+	text = translateTag(text, "_", "<em>", "</em>");
+	text = translateTag(text, "#", "<span style='font-size: 18pt;'><strong>", "</strong></span>");	
+	return text;
+}
+
+function translateTag(token, mark, openingTag, closingTag) {
 	var parts = token.split(mark);
 	for (j = parts.length - 1; j >= 0; j--) {
 		if (j % 2 != 0) {
@@ -51,9 +67,8 @@ function changeCodeColor() {
 	createHtml();
 }
 
-function htmlLinkFrom(match) {
-	var tokens = match.split(/[\[\(\)\]]/).filter(n => n != "");
-	return "<a style='color: #396;' href='" + tokens[1] + "'>" + tokens[0] + "</a>";
+function htmlLink(text, url) {
+	return "<a style='color: #396;' href='" + url + "'>" + text + "</a>";
 }
 
 function copyToClipboard() {
@@ -79,5 +94,4 @@ function copyToClipboard() {
 			document.getElementById("first").focus();
         }
 	}
-	
 }
